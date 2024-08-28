aae1b23484ceeace2d96bd06d072c8fd77ed56b2
Eleanor Wright <elliewright055@gmail.com>
2024-08-28 13:19:07 -0500

    Import non-JS files

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react"
 import JSXBasics from "./components/JSXBasics"
 import CreatingComponents from "./components/CreatingComponents"
 import Props, { RenderChildFunc, ToDoListItemComponent, NameClass, ToDoListItemClass } from "./components/Props"
+import user from './user.json'
+import './styles.css'
+import img from './img.jpg'
 
 function App() {
   return (
     <div>
-      <ToDoListItemClass isComplete>
-        Do dishes
-      </ToDoListItemClass>
+      {JSON.stringify(user)}
+      <img src={img}></img>
     </div>
   )
 }
