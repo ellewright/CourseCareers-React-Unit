1a32a316741ad7cecbc24901588fa3e78b2b43dd
Eleanor Wright <elliewright055@gmail.com>
2024-08-29 10:26:48 -0500

    Complete UseEffectExercise component

--- src/components/ComponentLifecycle/UseEffectHook.jsx
+++ src/components/ComponentLifecycle/UseEffectHook.jsx
@@ -40,3 +40,67 @@ export default function UseEffectHook() {
         </div>
     )
 }
+
+export function UseEffectExercise() {
+    const [show, setShow] = useState(true)
+
+    const childComponent = show ? <Child /> : null
+
+    return (
+        <div>
+            <button onClick={() => setShow(currentShow => !currentShow)}>
+                Show/Hide
+            </button>
+            {childComponent}
+        </div>
+    )
+}
+
+function Child() {
+    const [age, setAge] = useState(0)
+    const [name, setName] = useState("")
+
+    // useEffect(() => {
+    //     console.log("Hi.")
+
+    //     return () => {
+    //         console.log("Bye.")
+    //     }
+    // }, [])
+
+    // useEffect(() => {
+    //     console.log("Render.")
+    // })
+
+    // useEffect(() => {
+    //     console.log(`My name is ${name}, and I am ${age} years old.`)
+    // }, [name, age])
+
+    // useEffect(() => {
+    //     document.title = name
+    // }, [name])
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            console.log(`My name is ${name}.`)
+        }, 1000)
+
+        return () => {
+            clearTimeout(timeout)
+        }
+    }, [name])
+
+    return (
+        <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
+            <br />
+            <br />
+            <button onClick={() => setAge(a => a - 1)}>-</button>
+            {age}
+            <button onClick={() => setAge(a => a + 1)}>+</button>
+            <br />
+            <br />
+            My name is {name} and I am {age} years old.
+        </div>
+    )
+}
